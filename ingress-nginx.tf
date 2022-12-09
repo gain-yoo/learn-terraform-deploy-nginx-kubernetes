87fe05497d66a7720a94eec15cd912836df1e099
gain-yoo <100563973+gain-yoo@users.noreply.github.com>
2022-12-10 01:58:52 +0900

    ingress-nginx 배포

---
+++ ingress-nginx.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "ingress-nginx"
+    labels = {
+      App = "ScalableNginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginx"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
